3756f75253851d8c7a382a6c05830f272273500c
Leo Quinteros <leojquinteros@gmail.com>
2023-12-13 21:30:32 +0000

    refactor

--- flickr-sample/App/flickr_sampleApp.swift
+++ flickr-sample/App/flickr_sampleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct flickr_sampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView(viewModel: .init())
+            ContentView(viewModel: ContentViewModel())
         }
     }
 }
